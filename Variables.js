379ee0ad4d205b68dae54e23d6a90619e4696011
rotem huga <rotem.3007@gmail.com>
2022-12-19 09:44:45 +0200

    variables.js

---
+++ Variables.js
@@ -0,0 +1,44 @@
+//A
+let someName = "rotem";
+console.log(someName);
+someName = "Huga";
+// alert (someName)
+
+//B
+let number = Math.random();
+console.log(number);
+let z = number * 3;
+console.log(z);
+let calcu = `${number} * 3 = ${z}`;
+console.log(calcu);
+
+//C
+let favoriteNumber = 4;
+let favoriteStringed =`${favoriteNumber}`;
+console.log(favoriteStringed)
+
+//D
+const FAVORITENUMBER = 4;
+const halfOfFavNum = FAVORITENUMBER / 2
+console.log(halfOfFavNum)
+let michaelsFavorite = 13;
+let substract = michaelsFavorite - FAVORITENUMBER
+console.log(substract)
+let multy = michaelsFavorite * 26 
+console.log (multy)
+
+//E
+let children = "0";
+let partnersName = "asaf";
+let Location = "beqoa";
+let JOB = "laywer";
+let fortune = `I will be a ${JOB} in ${Location}, and married to ${partnersName} with ${children} kids`;
+console.log(fortune);
+
+//F
+let birth = "2000";
+let future = "2035";
+let calcu1 = future - birth
+console.log(calcu1);
+let output = `I will be either ${calcu1} or ${--calcu1} in ${future}`;
+console.log(output);
